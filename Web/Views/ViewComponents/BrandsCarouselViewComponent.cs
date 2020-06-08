0f802ef9ccdfaf84453f2b104234a75fb6a00923
Bartosz Wójcik <bwoj02@si-consulting.pl>
2020-06-08 20:38:45 +0200

    Brands carousel moved to ViewComponent

---
+++ Web/Views/ViewComponents/BrandsCarouselViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Data.Models;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Views.ViewComponents
+{
+    public class BrandsCarouselViewComponent : ViewComponent
+    {
+        private readonly IRepository<Brand> _brandRepository;
+        public BrandsCarouselViewComponent(IRepository<Brand> brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = _brandRepository.Get().OrderByDescending(b=>b.EditDate).Take(3);
+
+            return View("BrandsCarouselComponent", model);
+        }
+    }
+}
