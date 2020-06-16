910907498f9c5cd7dd105da58f775b1424f72749
Bartosz Wójcik <bwoj02@si-consulting.pl>
2020-06-16 20:22:37 +0200

    CreateBrand first attempt

---
+++ Web/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Models;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IRepository<Brand> _brandRepository;
+        
+        public AdminController(IRepository<Brand> brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateBrand() => View();
+    }
+}
